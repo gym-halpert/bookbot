ca46768d5a34c6e7f84b7f065511bc3d9ff96f58
Gym-Halpert <11765958-Gym-Halpert@users.noreply.gitlab.com>
2025-12-11 23:04:26 -0700

    char count functions added to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,4 +9,8 @@ def main():
     file_content = get_book_text("books/frankenstein.txt")
     total = get_num_words(file_content)
     print(f"Found {total} total words")
+    char_counts = get_chars(file_content)
+    for k, v in char_counts.items():
+        print(f"'{k}': {v}")
+
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_num_words(file_content):
     for i in file_content.split():
         total += 1
     return total
+
+def get_chars(file_content):
+    char_counts = {}
+    for word in file_content.split():
+        for char in word:
+            ch = char.lower()
+            if ch not in char_counts:
+                char_counts[ch] = 1
+            else:
+                char_counts[ch] += 1
+    return char_counts
